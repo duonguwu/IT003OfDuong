ea75b34520c7c067fbba9efe72f317c20b73e4e1
Nguyễn Dương <97208260+duonguwu@users.noreply.github.com>
2022-04-13 20:19:16 +0700

    Update NodeInt.cpp

--- Thực hành/NodeInt.cpp
+++ Thực hành/NodeInt.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 
-struct Node
+struct Node  // Khai báo Node
 {
     int info;
     Node *next;
 };
 
-struct List{
-    Node *head;
+struct List{  // Khai báo list
+    Node *head; 
     Node *tail;
 };
 
-void CreateEmptyList(List &l){
+void CreateEmptyList(List &l){ // Tạo danh sách rỗng
     l.head = NULL;
     l.tail = NULL;
 }
 
-Node *CreateNode(int x){
+Node *CreateNode(int x){ // Tạo Node
     Node *p = new Node;
     if (p == NULL)
         exit(1);
@@ -27,7 +27,7 @@ Node *CreateNode(int x){
     return p;
 }
 
-void PrintList(List l){
+void PrintList(List l){ // In danh sách
     Node *p;
     if (l.head == NULL){
         cout << "Rong.";
@@ -41,10 +41,11 @@ void PrintList(List l){
     }
 }
 
-void AddHead(List &l, Node *p){
-    if(l.head == NULL){
+void AddHead(List &l, Node *p){  // Thêm đầu
+    if(l.head == NULL){     // Phải kiểm tra danh sách có rỗng hay không
         l.head = p;
         l.tail = l.head;
+        // hoặc là l.head = l.tail = p;
     }
     else{
         p->next = l.head;
@@ -52,8 +53,8 @@ void AddHead(List &l, Node *p){
     }
 }
 
-void AddTail(List &l, Node *p){
-    if(l.head = NULL){
+void AddTail(List &l, Node *p){ // Thêm cuối
+    if(l.head = NULL){   // Kiểm tra tương tự thêm đầu
         l.head = p;
         l.tail = l.head;
     }
@@ -63,14 +64,35 @@ void AddTail(List &l, Node *p){
     }
 }
 
-void CreateList(List &l, int n)
+void CreateList(List &l)  // Nhập 
 {
+    int n;
+    cout << "Nhap vao so luong Node: ";
+    cin >> n;
     int i = 1;
     while (i <= n){
         int x;
         cin >> x;
         AddHead(l, CreateNode(x));
         i++;
+    }
+}
+
+void CreateListbyKeyBoard(List &l)  // Nhập từ bàn phím, bấm -99 để ngừng nhập
+{
+    cout << " Nhap danh sach, nhap -99 de ket thuc.\n";
+    bool i = true;
+    while (i == true)
+    {
+        int x;
+        cout << "Nhap node: ";
+        cin >> x;
+        if (x == -99)
+        {
+            i = false;
+            continue;
+        }
+        AddHead(l, CreateNode(x));
     }
 }
 
